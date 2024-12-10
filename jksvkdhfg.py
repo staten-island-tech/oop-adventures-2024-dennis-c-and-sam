d0dba7a06455c540bb974ef20f37eb8b8eedc88f
Samqiu3 <samq3@nycstudents.net>
2024-12-10 09:26:21 -0500

    12/10

--- jksvkdhfg.py
+++ jksvkdhfg.py
@@ -5,3 +5,16 @@ screen_width = 800
 screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('game')
+
+# Variable to keep our game loop running 
+running = True
+  
+# game loop 
+while running: 
+    
+# for loop through the event queue   
+    for event in pygame.event.get(): 
+      
+        # Check for QUIT event       
+        if event.type == pygame.QUIT: 
+            running = False
