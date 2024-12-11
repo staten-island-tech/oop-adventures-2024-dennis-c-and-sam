82a2ed07ca5e87e632abab1895d758c3e56f5421
Samqiu3 <samq3@nycstudents.net>
2024-12-11 09:24:31 -0500

    12/11

--- jksvkdhfg.py
+++ jksvkdhfg.py
@@ -6,6 +6,17 @@ screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('game')
 
+WHITE = (255, 255, 255)
+BLACK = (0,0,0)
+
+x = 200
+y = 200
+
+width = 20
+height = 20
+
+vel=5
+
 # Variable to keep our game loop running 
 running = True
   
@@ -18,3 +29,28 @@ while running:
         # Check for QUIT event       
         if event.type == pygame.QUIT: 
             running = False
+
+keys = pygame.key.get_pressed() 
+	
+	# if left arrow key is pressed 
+if keys[pygame.K_a] and x>0: 
+		
+		# decrement in x co-ordinate 
+		x -= vel 
+		
+	# if left arrow key is pressed 
+if keys[pygame.K_d] and x<500-width: 
+		
+		# increment in x co-ordinate 
+		x += vel 
+		
+	# if left arrow key is pressed 
+if keys[pygame.K_w] and y>0: 
+		
+		# decrement in y co-ordinate 
+		y -= vel 
+		
+	# if left arrow key is pressed 
+if keys[pygame.K_s] and y<500-height: 
+		# increment in y co-ordinate 
+		y += vel 
