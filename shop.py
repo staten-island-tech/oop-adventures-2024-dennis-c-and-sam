fe4dd5b01fe0c30affcd8e908d79b7968a075369
Samqiu3 <samq3@nycstudents.net>
2024-12-16 09:25:45 -0500

    12/16

--- shop.py
+++ shop.py
@@ -1,7 +1,11 @@
 import json
-test= open("./items.json", encoding="utf8")
+import os
+test= open("items.json", encoding="utf8")
 items= json.load(test)
 import random
+
+with open ("shop.json", "r", encoding='utf-8-sig') as f:
+    shop_items = json.load(f)
 
 class shop:
     def __init__(self, item: str, description:str , price: int):
@@ -11,3 +15,5 @@ class shop:
 
 ranitem = random.choice(items)
 print (ranitem)
+
+shop_items.append(ranitem)
