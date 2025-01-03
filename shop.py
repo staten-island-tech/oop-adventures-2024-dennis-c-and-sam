b0d0fbebb9b794631a49ffa16080924da4a2fb23
Samqiu3 <samq3@nycstudents.net>
2025-01-03 08:59:46 -0500

    1/3

--- shop.py
+++ shop.py
@@ -1,10 +1,28 @@
-class shop: 
-    def __init__(self,item, )
-    
+import random
 
 class item:
         def __init__(self, item: str, description:str , price: int):
             self.item = item
             self.description= description
             self.price= price
+
+        item_dict = [{ "speed_potion",  "Increases speed", 5 },
+                    { "ladder","allows you jump over one wall", 10},
+                    {"slingshot",  "stuns monster for a short amount of time", 15}]
+        
+        shop_items=[]
+
+        for i in range(3):
+            item_random = random.choice(item_dict)
+            shop_items.append(item_random)
+
+            print(shop_items)
+
+def buy(self,item,inventory):
+        self.inventory= inventory
+        inventory={}
+        self.inventory.append(item)
+
+
+
 
