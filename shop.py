2a6fe41cb8435d84978f72b709b485014775ccbf
Samqiu3 <samq3@nycstudents.net>
2025-01-06 09:24:52 -0500

    1/6

--- shop.py
+++ shop.py
@@ -1,30 +1,30 @@
 import random
 
 class item:
-        def __init__(self, item: str, description:str , price: int):
+        def __init__(self, item: str, description:str , price: int,):
             self.item = item
             self.description= description
             self.price= price
 
-
-        item_dict = [{ "speed_potion",  "Increases speed", 5 },
-                    { "ladder","allows you jump over one wall", 10},
-                    {"slingshot",  "stuns monster for a short amount of time", 15}]
+item_dict = [( "speed_potion",  "Increases speed", 5 ),
+                    ( "ladder","allows you jump over one wall", 10),
+                   ("dash",  "makes you dash in a certain direction", 5)]
         
-        shop_items=[]
+shop_items=[]
 
-        for i in range(3):
+for i in range(3):
             item_random = random.choice(item_dict)
             shop_items.append(item_random)
 
-            print(shop_items)
+while shopping != "done":
+        shopping=input("what would you like to buy? type done if done shopping.").capitalize()
 
-def buy(self,item,inventory):
-        self.inventory= inventory
-        inventory={}
-        self.inventory.append(item)
-
-
-
-
+class mc():
+    def __init__(self,inventory,money,speed):
+        self.inventory = inventory
+        inventory=[]
+        self.money = money
+        self.speed = speed
+    def buy(self, item):
+           self.inventory.append(item)
 
