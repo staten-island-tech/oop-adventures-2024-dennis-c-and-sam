952d23a8c1779049454a4af4d5927f9d8dc43ae4
Samqiu3 <samq3@nycstudents.net>
2025-01-09 18:07:57 -0500

    aaaaaa

--- jksvkdhfg.py
+++ jksvkdhfg.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 pygame.init()
 
 screen_width = 800
@@ -15,42 +16,18 @@ y = 200
 width = 20
 height = 20
 
-vel=5
-
-# Variable to keep our game loop running 
 running = True
-  
-# game loop 
-while running: 
-    
-# for loop through the event queue   
-    for event in pygame.event.get(): 
-      
-        # Check for QUIT event       
-        if event.type == pygame.QUIT: 
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
             running = False
 
-    keys = pygame.key.get_pressed() 
-	
-	# if left arrow key is pressed 
-    if keys[pygame.K_a] and x>0: 
-		
-		# decrement in x co-ordinate 
-		    x -= vel 
-		
-	# if left arrow key is pressed 
-    if keys[pygame.K_d] and x<500-width: 
-		
-		# increment in x co-ordinate 
-		    x += vel 
-		
-	# if left arrow key is pressed 
-    if keys[pygame.K_w] and y>0: 
-		
-		# decrement in y co-ordinate 
-		    y -= vel 
-		
-	# if left arrow key is pressed 
-    if keys[pygame.K_s] and y<500-height: 
-		# increment in y co-ordinate 
-		    y += vel 
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_1:  # Select Rhythm Game
+                current_game = "rhythm_game"
+                os.system("python shop.py")
+
+    screen.fill(WHITE)
+    pygame.display.flip()
+
+pygame.quit()
