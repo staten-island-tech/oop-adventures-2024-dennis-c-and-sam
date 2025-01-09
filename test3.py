419e56d63571a0e25d59be2ed1317d325ff7d146
Samqiu3 <samq3@nycstudents.net>
2025-01-08 20:04:14 -0500

    aaaaaaaaaaaaaa

--- test3.py
+++ test3.py
@@ -1,23 +1,25 @@
 import random
 
 class Item:
-        def __init__(self, name: str, description:str , price: int,):
-            self.name = name
-            self.description= description
-            self.price= price
+    def __init__(self, name: str, description: str, price: int):
+        self.name = name
+        self.description = description
+        self.price = price
 
-item_dict = [( "speed_potion",  "Increases speed", 5 ),
-                    ( "ladder","allows you jump over one wall", 10),
-                   ("dash",  "makes you dash in a certain direction", 5)]
-        
-shop_items=[]
+item_dict = [
+    ("speed_potion", "Increases speed", 5),
+    ("ladder", "Allows you to jump over one wall", 10),
+    ("dash", "Makes you dash in a certain direction", 5)
+]
+
+shop_items = []
 
 for i in range(3):
-            item_random = random.choice(item_dict)
-            shop_items.append(Item(*item_random))
+    item_random = random.choice(item_dict)
+    shop_items.append(Item(*item_random))
 
-class player():
-    def __init__(self,inventory,money,speed):
+class Player:
+    def __init__(self, inventory, money, speed):
         self.inventory = inventory
         self.money = money
         self.speed = speed
@@ -29,23 +31,23 @@ class player():
             print(f"You have bought {item.name}. You now have {self.money} money left.")
         else:
             print("You don't have enough money for this item!")
-        
-mc = player(inventory=[],money=100,speed=10)
+
+mc = Player(inventory=[], money=100, speed=10)
 
 shopping = ""
-while shopping.lower() != "done":
+while shopping.capitalize() != "Done":
     print("Items available for sale:")
     for items in shop_items:
         print(f"{items.name}: {items.description} - Price: {items.price}")
 
-    shopping = input("What would you like to buy? Type 'done' if done shopping: ").lower()
+    shopping = input("What would you like to buy? Type 'done' if done shopping: ").capitalize()
 
-    if shopping == "done":
+    if shopping.capitalize() == "Done":
         break
 
     item_found = False
     for items in shop_items:
-        if items.name.lower() == shopping:
+        if items.name.capitalize() == shopping.capitalize():
             mc.buy(items)
             item_found = True
             break
